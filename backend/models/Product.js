472904b0e302c182d1b1586dc9474296319cee1c
Dario Coletto <darocoletto@gmail.com>
2025-07-02 12:51:17 -0300

    model ampliado

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,6 +1,15 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../database');
 
+/**
+ * Product model representing a product in the e-commerce application.
+ * 
+ * @module models/Product
+ * @requires sequelize
+ * @description
+ * This model defines the structure of the Product entity in the database,
+ * including fields for id, name, price, brand, category, stock, description, and imageUrl.
+ */
 const Product = sequelize.define('Product', {
   id: {
     type: DataTypes.INTEGER,
@@ -23,6 +32,13 @@ const Product = sequelize.define('Product', {
     type: DataTypes.STRING,
     allowNull: false
   },
+
+  stock: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 0
+  },
+
   description: {
     type: DataTypes.TEXT,
     allowNull: true
