81fd6601e8059c846ba0ebf87dd05214819a26b5
Dario Coletto <darocoletto@gmail.com>
2025-07-02 13:01:59 -0300

    schema product created

---
+++ backend/schema/productSchema.js
@@ -0,0 +1,36 @@
+const Joi = require('joi');
+/**
+ * Product schema for validating product data.
+ * 
+ * @module schema/productSchema
+ * @description
+ * This schema defines the structure and validation rules for product data,
+ * including fields for id, name, price, brand, category, stock, description, and imageUrl.
+ */
+
+// Joi for create Product
+const productCreateSchema = Joi.object({
+  name: Joi.string().required(),
+  price: Joi.number().required(),
+  brand: Joi.string().required(),
+  category: Joi.string().required(),
+  stock: Joi.number().integer().min(0).default(0),
+  description: Joi.string().allow(''),
+  imageUrl: Joi.string().uri().allow('')
+});
+
+// Joi for update Product
+const productUpdateSchema = Joi.object({
+    name: Joi.string().optional(),
+    price: Joi.number().optional(),
+    brand: Joi.string().optional(),
+    category: Joi.string().optional(),
+    stock: Joi.number().integer().min(0).default(0).optional(),
+    description: Joi.string().allow('').optional(),
+    imageUrl: Joi.string().uri().allow('').optional()
+    });
+
+module.exports = {
+  productCreateSchema,
+  productUpdateSchema
+};    
