ece9dd7de66d5a8dd1cc2e090592ef52efa6647d
Dario Coletto <darocoletto@gmail.com>
2025-07-02 12:28:15 -0300

    modelo ampliado

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -2,6 +2,11 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../database');
 
 const Product = sequelize.define('Product', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
   name: {
     type: DataTypes.STRING,
     allowNull: false
@@ -9,7 +14,24 @@ const Product = sequelize.define('Product', {
   price: {
     type: DataTypes.FLOAT,
     allowNull: false
+  },
+  brand: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  cathegory: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  imageUrl: {
+    type: DataTypes.STRING,
+    allowNull: true
   }
+
 });
 
 module.exports = Product;
