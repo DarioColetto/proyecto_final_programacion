f9a47b3b626b424e47c899070e31294f89dfb9b8
Dario Coletto <darocoletto@gmail.com>
2025-07-02 12:52:26 -0300

    Service updated

--- backend/services/productService.js
+++ backend/services/productService.js
@@ -1,9 +1,55 @@
 const repository = require('../repositories/productRepository');
 
-module.exports = {
-  getAll: () => repository.findAll(),
-  getById: (id) => repository.findById(id),
-  create: (data) => repository.create(data),
-  update: (id, data) => repository.update(id, data),
-  delete: (id) => repository.remove(id),
-};
+/**
+ * Service for managing products.
+ * Provides methods to perform CRUD operations on products.
+ * @class ProductService
+ * @module services
+ * @requires repositories/productRepository
+ */
+
+class ProductService {
+
+  static getAll() {
+    return repository.findAll();
+  }
+
+  static getById(id) {
+    return repository.findById(id);
+  }
+
+  static getByName(name) {
+    return repository.findByName(name);
+  }
+
+  static getByBrand(brand) {
+    return repository.findByBrand(brand);
+  }
+
+  static getByCategory(category) {
+    return repository.findByCategory(category);
+  }
+
+  static getByPriceRange(minPrice, maxPrice) {
+    return repository.findByPriceRange(minPrice, maxPrice);
+  }
+
+  static getByStock(stock) {
+    return repository.findByStock(stock);
+  }
+
+  static create(data) {
+    return repository.create(data);
+  }
+
+  static update(id, data) {
+    return repository.update(id, data);
+  }
+
+  static delete(id) {
+    return repository.remove(id);
+  }
+}
+
+
+module.exports = ProductService;
