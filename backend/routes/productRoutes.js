529f0e078c703ec71dacd9897182e2b1b190bc0f
Dario Coletto <darocoletto@gmail.com>
2025-07-02 13:14:09 -0300

    update routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const controller = require('../controllers/productController');
+const validate = require('../middlewares/validate');
+const { productCreateSchema, productUpdateSchema } = require('../schema/productSchema');
 const router = express.Router();
 
 router.get('/', controller.getAll);
 router.get('/:id', controller.getById);
-router.post('/', controller.create);
-router.put('/:id', controller.update);
+router.get('/name/:name', controller.getByName);
+router.get('/brand/:brand', controller.getByBrand);
+router.get('/category/:category', controller.getByCategory);
+router.get('/price-range', controller.getByPriceRange);
+router.post('/',validate(productCreateSchema) ,controller.create);
+router.put('/:id', validate(productUpdateSchema)  ,controller.update);
 router.delete('/:id', controller.delete);
 
 module.exports = router;
