4382f4e23ba2d31d2e65f01d1bea9c02392769d0
Dario Coletto <darocoletto@gmail.com>
2025-07-08 00:13:13 -0300

    controller updated

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -8,6 +8,24 @@ exports.getById = async (req, res) => {
   res.json(await service.getById(req.params.id));
 };
 
+exports.getByName = async (req, res) => { // <-- Añadir esta función
+  res.json(await service.getByName(req.params.name)); // Suponiendo un método getByName en el servicio
+};
+
+exports.getByBrand = async (req, res) => { // <-- Añadir esta función
+    res.json(await service.getByBrand(req.params.brand)); // Suponiendo un método getByBrand en el servicio
+};
+
+exports.getByCategory = async (req, res) => { // <-- Añadir esta función
+    res.json(await service.getByCategory(req.params.category)); // Suponiendo un método getByCategory en el servicio
+};
+
+exports.getByPriceRange = async (req, res) => { // <-- Añadir esta función
+    // Ejemplo de cómo manejar los parámetros de rango de precios (query params)
+    const { minPrice, maxPrice } = req.query;
+    res.json(await service.getByPriceRange(minPrice, maxPrice)); // Suponiendo un método getByPriceRange en el servicio
+};
+
 exports.create = async (req, res) => {
   res.status(201).json(await service.create(req.body));
 };
